657b62c2a0337d98effc7118fc605d34644208ac
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2025-08-16 08:04:52 -0600

    refactor(settings): Modernize production settings

--- src/namespace/settings/produccion.py
+++ src/namespace/settings/produccion.py
@@ -4,20 +4,14 @@ DEBUG=False
 #DEBUG=True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDER_PROTOCOL', 'https')
 
-ALLOWED_HOSTS = ['namespace.mx', '192.3.90.48', 'hydra.namespace.mx', 'www.namespace.mx', '127.0.0.1', 'localhost']
+# ALLOWED_HOSTS is now managed via the .env file in base.py
 INTERNAL_IPS = ("127.0.0.1",)
 
 
 
+# Caching configuration is now managed via the .env file
 CACHES = {
-    "default": {
-        "BACKEND": "redis_cache.cache.RedisCache",
-        "LOCATION": "127.0.0.1:6379:1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "redis_cache.client.DefaultClient",
-            # "PASSWORD": "secretpassword", # Optional
-        }
-    }
+    'default': env.cache('CACHE_URL'),
 }
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
