c5ddf052b469d6941910c6b409d6e61bc6473664
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2025-08-16 09:30:51 -0600

    refactor(urls): Update URL patterns to Django 4.2 syntax

--- src/namespace/urls.py
+++ src/namespace/urls.py
@@ -1,44 +1,48 @@
 #-*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
+from django.urls import include, path, re_path
 from django.views.decorators.cache import cache_page
+from django.conf import settings
+from django.conf.urls.static import static # For static files in debug mode
 
 from blog.views import BlogArchivo, EntradaIndividual, CategoriaList, TagListView
 from blog.views import BlogFeed
 from blog.views import BlogSitemap
 
 from django.contrib import admin
-admin.autodiscover()
+# admin.autodiscover() is no longer needed in modern Django
 
 sitemaps = { "blog": BlogSitemap }
 
-urlpatterns = patterns('',
-    # url(r'^$', BlogArchivo.as_view(), name='home'),
-    url(r'^$', cache_page(30)(BlogArchivo.as_view()), name='home'),
-    url(r'^mapa/$', 'core.views.mapa', name='mapa'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^rss/$', BlogFeed()),
+urlpatterns = [
+    # path('', BlogArchivo.as_view(), name='home'), # Original commented out
+    re_path(r'^', cache_page(30)(BlogArchivo.as_view()), name='home'),
+    path('mapa/', 'core.views.mapa', name='mapa'), # This will need to be fixed, direct string import is deprecated
+    path('admin/', admin.site.urls),
+    path('rss/', BlogFeed()),
 
     # ### Entrada Individual ### #
-    # url(r'^(?P<cat>[-\w]+)/(?P<slug>[-\w]+)/$', EntradaIndividual.as_view(), name='post'),
-    url(r'^(?P<cat>[-\w]+)/(?P<slug>[-\w]+)/$', cache_page(30) (EntradaIndividual.as_view()), name='post'),
-    url(r'^t/(?P<slug>[-\w]+)/\d+$', EntradaIndividual.as_view()),
+    # re_path(r'^(?P<cat>[-\w]+)/(?P<slug>[-\w]+)/
+, EntradaIndividual.as_view(), name='post'), # Original commented out
+    re_path(r'^(?P<cat>[-\w]+)/(?P<slug>[-\w]+)/
+, cache_page(30) (EntradaIndividual.as_view()), name='post'),
+    re_path(r'^t/(?P<slug>[-\w]+)/\d+
+, EntradaIndividual.as_view()),
     
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-    url(r'^robots\.txt$', 'core.views.robots'),
+    path('sitemap.xml', include('django.contrib.sitemaps.views.sitemap')), # include() for sitemap
+    path('robots.txt', 'core.views.robots'),
 
     ### Etiquetas
-    url(r'^tag/$', 'blog.views.tags_list', name='tags'),
-    url(r'^tag/(?P<tag_slug>[-\w]+)$', TagListView.as_view(), name='tag'),
+    path('tag/', 'blog.views.tags_list', name='tags'),
+    re_path(r'^tag/(?P<tag_slug>[-\w]+)
+, TagListView.as_view(), name='tag'),
 
-    url(r'^([-\w]+)/$', CategoriaList.as_view(), name="categoria"),
-)
+    re_path(r'^([-\w]+)/
+, CategoriaList.as_view(), name="categoria"),
+]
 
-error404 = "core.views.error404"
+# error404 = "core.views.error404" # This is not how 404 handlers are defined anymore
 
-from django.conf import settings
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-   )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # The old way of serving static files in debug mode is deprecated.
+    # We also need to define MEDIA_URL in settings.
