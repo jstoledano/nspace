55421bf4df139c01d6fcf3a6495863d34229e4cc
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2025-08-16 07:52:39 -0600

    refactor(settings): Update local.py for Django 4.2 compatibility

--- src/namespace/settings/local.py
+++ src/namespace/settings/local.py
@@ -2,18 +2,13 @@ __author__ = 'Javier Sanchez'
 from .base import *
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 
 
 INTERNAL_IPS = ("127.0.0.1",)
 
-INSTALLED_APPS += ("debug_toolbar", )
-INTERNAL_IPS = ("127.0.0.1",)
-MIDDLEWARE_CLASSES += (
-    # 'annoying.middlewares.StaticServe',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-)
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
 CACHES = {
     'default': {
